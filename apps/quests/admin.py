e18442c12a006ea9d9b5a46ece5e12f5e298b918
Last G <last_g@hackerdom.ru>
2014-03-08 21:32:42 +0600

    Admin interface

--- apps/quests/admin.py
+++ apps/quests/admin.py
@@ -1,16 +1,32 @@
-from django.db.models import get_models, get_app
 from django.contrib import admin
-from django.contrib.admin.sites import AlreadyRegistered
 
+from . import models
 # Register your models here.
 
+class QuestAdmin(admin.ModelAdmin):
+    fields = (
+        'shortname',
+        ('provider_type', 'provider_file'),
+        ('is_simple', 'is_manual')
+    )
+    list_display = ('shortname', 'category', 'score')
+    list_filter = ('category', 'score')
 
-def autoregister(*app_list):
-    for app_name in app_list:
-        app_models = get_app(app_name)
-        for model in get_models(app_models):
-            try:
-                admin.site.register(model)
-            except AlreadyRegistered:
-                pass
-autoregister('quests')
+class QuestVariantAdmin(admin.ModelAdmin):
+    list_display = ('team', 'quest')
+    list_filter = ('team', 'quest')
+
+class QuestAnswerAdmin(admin.ModelAdmin):
+    list_display = ('quest_name', 'team', 'is_checked', 'is_success')
+    list_filter = ('quest_variant__quest', 'quest_variant__team', 'is_checked', 'is_success')
+    def quest_name(self, obj):
+        return obj.quest_variant.quest
+    quest_name.short_description = 'Quest'
+
+    def team(self, obj):
+        return obj.quest_variant.team
+    team.short_description = 'Team'
+
+admin.site.register(models.Quest, QuestAdmin)
+admin.site.register(models.QuestVariant, QuestVariantAdmin)
+admin.site.register(models.QuestAnswer, QuestAnswerAdmin)
