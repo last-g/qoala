1a028e3ac0d2a218b0bd9caeaef43e7c42b345b4
Last G <last_g@hackerdom.ru>
2014-03-08 22:20:07 +0600

    Open for quest admin

--- apps/quests/admin.py
+++ apps/quests/admin.py
@@ -7,16 +7,19 @@ class QuestAdmin(admin.ModelAdmin):
     fields = (
         'shortname',
         ('provider_type', 'provider_file'),
-        ('is_simple', 'is_manual')
+        ('is_simple', 'is_manual'),
+        'open_for'
     )
     list_display = ('shortname', 'category', 'score')
     list_filter = ('category', 'score')
 
 class QuestVariantAdmin(admin.ModelAdmin):
+    fields = (('team', 'quest'), 'timeout', ('is_valid', 'try_count'))
     list_display = ('team', 'quest')
     list_filter = ('team', 'quest')
 
 class QuestAnswerAdmin(admin.ModelAdmin):
+    fields = ('quest_variant', ('score', 'is_checked', 'is_success'), 'answer', 'result')
     list_display = ('quest_name', 'team', 'is_checked', 'is_success')
     list_filter = ('quest_variant__quest', 'quest_variant__team', 'is_checked', 'is_success')
     def quest_name(self, obj):
