e8d79da08ccadda83162640e68f71cb9e6439de5
Last G <last_g@hackerdom.ru>
2014-03-09 01:00:11 +0600

    Force login checks

--- apps/quests/views.py
+++ apps/quests/views.py
@@ -1,4 +1,5 @@
 import django
+from django.contrib.auth.decorators import login_required
 from django.http.response import HttpResponseForbidden
 from django.shortcuts import render, redirect, get_object_or_404
 from django.template import response
@@ -17,11 +18,13 @@ def open_task(req, task):
     return render(req, 'quests/open.html', {'variant': variant, 'form': forms.AnswerForm()})
 
 
+@login_required
 def open_task_by_id(req, task_id):
     task = get_object_or_404(models.Quest, pk=task_id)
     return open_task(req, task)
 
 
+@login_required
 def open_task_by_name(req, task_name):
     task = get_object_or_404(models.Quest, shortname=task_name)
     return open_task(req, task)
@@ -43,11 +46,13 @@ def answer_task(request, task):
         return redirect(task.get_absolute_url())
 
 
+@login_required
 def answer_task_by_id(request, task_id):
     task = get_object_or_404(models.Quest, pk=task_id)
     return answer_task(request, task)
 
 
+@login_required
 def answer_task_by_name(request, task_name):
     task = get_object_or_404(models.Quest, shortname=task_name)
     return answer_task(request, task)
@@ -59,16 +64,19 @@ def get_static(request, task, path):
     return redirect(new_path)
 
 
+@login_required
 def get_task_static_by_id(request, task_id, path):
     task = get_object_or_404(models.Quest, pk=task_id)
     return get_static(request, task, path)
 
 
+@login_required
 def get_task_static_by_name(request, task_name, path):
     task = get_object_or_404(models.Quest, shortname=task_name)
     return get_static(request, task, path)
 
 
+@login_required
 def check_answer(request, answer_id):
     answer = get_object_or_404(models.QuestAnswer, pk=answer_id, quest_variant__team=request.user)
     return render(request, 'quests/show_answer.html', {'answer': answer})
