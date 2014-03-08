2694e9fb364e30947c35139a719020a018f0c643
Last G <last_g@hackerdom.ru>
2014-03-08 23:34:37 +0600

    Apdmin improvements

--- apps/quests/admin.py
+++ apps/quests/admin.py
@@ -6,22 +6,35 @@ from . import models
 class QuestAdmin(admin.ModelAdmin):
     fields = (
         'shortname',
+        ('category', 'score'),
         ('provider_type', 'provider_file'),
         ('is_simple', 'is_manual'),
-        'open_for'
+        'open_for', 'provider_hash'
     )
+    readonly_fields = ('provider_type', 'provider_hash', 'category', 'score')
     list_display = ('shortname', 'category', 'score')
     list_filter = ('category', 'score')
+    list_select_related = ('category',)
+    filter_horizontal = ('open_for',)
+
 
 class QuestVariantAdmin(admin.ModelAdmin):
-    fields = (('team', 'quest'), 'timeout', ('is_valid', 'try_count'))
+    fields = (('team', 'quest'),
+              'timeout',
+              ('is_valid', 'try_count'),
+              'html'
+    )
+    readonly_fields = ('html',)
     list_display = ('team', 'quest')
     list_filter = ('team', 'quest')
+    list_select_related = ('quest', 'teams')
 
 class QuestAnswerAdmin(admin.ModelAdmin):
     fields = ('quest_variant', ('score', 'is_checked', 'is_success'), 'answer', 'result')
-    list_display = ('quest_name', 'team', 'is_checked', 'is_success')
+    list_display = ('quest_name', 'team', 'is_checked', 'is_success', 'answer')
     list_filter = ('quest_variant__quest', 'quest_variant__team', 'is_checked', 'is_success')
+    list_select_related = ('quest_variant',)
+#    readonly_fields = ('quest_name', 'team')
     def quest_name(self, obj):
         return obj.quest_variant.quest
     quest_name.short_description = 'Quest'
