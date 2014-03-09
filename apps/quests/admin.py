05e2b1a56da627e562f81cd3cbd5a86d3a55d6a8
Last G <last_g@hackerdom.ru>
2014-03-09 11:02:17 +0600

    Added created and updated dates

--- apps/quests/admin.py
+++ apps/quests/admin.py
@@ -9,9 +9,10 @@ class QuestAdmin(admin.ModelAdmin):
         ('category', 'score'),
         ('provider_type', 'provider_file'),
         ('is_simple', 'is_manual'),
-        'open_for', 'provider_hash'
+        'open_for', 'provider_hash',
+        ('created_at', 'updated_at')
     )
-    readonly_fields = ('provider_type', 'provider_hash', 'category', 'score')
+    readonly_fields = ('provider_type', 'provider_hash', 'category', 'score', 'created_at', 'updated_at')
     list_display = ('shortname', 'category', 'score')
     list_filter = ('category', 'score')
     list_select_related = ('category',)
@@ -23,29 +24,40 @@ class QuestVariantAdmin(admin.ModelAdmin):
     fields = (('team', 'quest'),
               'timeout',
               ('is_valid', 'try_count'),
-              'html'
+              'html',
+              ('created_at', 'updated_at')
     )
-    readonly_fields = ('html',)
+    readonly_fields = ('html', 'created_at', 'updated_at')
     list_display = ('team', 'quest')
     list_filter = ('team', 'quest')
     list_select_related = ('quest', 'teams')
     search_fields = ('quest__shortname', 'team__name')
 
+
 class QuestAnswerAdmin(admin.ModelAdmin):
-    fields = ('quest_variant', ('score', 'is_checked', 'is_success'), 'answer', 'result')
+    fields = ('quest_variant',
+              ('score', 'is_checked', 'is_success'),
+              'answer',
+              'result',
+              ('created_at', 'updated_at')
+    )
     list_display = ('quest_name', 'team', 'is_checked', 'is_success', 'answer')
     list_filter = ('quest_variant__quest', 'quest_variant__team', 'is_checked', 'is_success')
     list_editable = ('is_checked', 'is_success')
     list_select_related = ('quest_variant',)
     search_fields = ('answer', 'quest_variant__quest__shortname')
-#    readonly_fields = ('quest_name', 'team')
+    readonly_fields = ('created_at', 'updated_at')
+
     def quest_name(self, obj):
         return obj.quest_variant.quest
+
     quest_name.short_description = 'Quest'
 
     def team(self, obj):
         return obj.quest_variant.team
+
     team.short_description = 'Team'
+
 
 admin.site.register(models.Quest, QuestAdmin)
 admin.site.register(models.QuestVariant, QuestVariantAdmin)
