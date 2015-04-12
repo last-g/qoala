4ac3e9b6a918216e15d2273098adf775d67d9bc5
Sergey Azovskov <last-g@yandex-team.ru>
2015-04-12 16:36:11 +0500

    Fixing deps

--- deploy/install_dependencies.sh
+++ deploy/install_dependencies.sh
@@ -3,5 +3,5 @@
 source `dirname $0`/common.sh
 
 sudo apt-get update
-sudo apt-get install -y python python-pip supervisor # python-setuptools  nginx gunicorn
+sudo apt-get install -y python python-pip supervisor python-psycopg2 # python-setuptools  nginx gunicorn
 sudo pip install -r ${PROJECT}/requirements.txt
